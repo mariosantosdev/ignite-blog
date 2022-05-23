afca79cf8ce3ad4c728874ccc532e0d3a742a6f0
Mário Santos <mariodev7@gmail.com>
2022-05-23 04:09:22 +0000

    add redirect page in post component

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,11 +1,13 @@
+import { useMemo } from 'react';
 import { FiCalendar, FiUser } from 'react-icons/fi';
 import { format } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
+import { useRouter } from 'next/router';
 
-import { useMemo } from 'react';
 import styles from './post.module.scss';
 
 interface PostProps {
+  slug: string;
   title: string;
   subtitle: string;
   date: string;
@@ -13,17 +15,26 @@ interface PostProps {
 }
 
 export default function Post({
+  slug,
   title,
   subtitle,
   date,
   author,
 }: PostProps): JSX.Element {
+  const router = useRouter();
+
   const formatedDate = useMemo(() => {
     return format(new Date(date), 'dd MMM yyyy', { locale: ptBR });
   }, [date]);
 
   return (
-    <div className={styles.postContainer}>
+    <div
+      role="button"
+      tabIndex={-1}
+      className={styles.postContainer}
+      onClick={() => router.push(`/post/${slug}`)}
+      aria-hidden="true"
+    >
       <h1>{title}</h1>
       <h2>{subtitle}</h2>
       <footer>

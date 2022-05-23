9529d15a3ede65fccf2895a7e20f5e14e44697ac
Mário Santos <mariodev7@gmail.com>
2022-05-23 04:14:59 +0000

    add anchor in header logo image

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,16 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 import styles from './header.module.scss';
 
 export default function Header(): JSX.Element {
   return (
     <div className={styles.container}>
-      <Image alt="Logo" src="/logo.svg" width={239} height={27} />
+      <Link href="/">
+        <a>
+          <Image alt="logo" src="/logo.svg" width={239} height={27} />
+        </a>
+      </Link>
     </div>
   );
 }

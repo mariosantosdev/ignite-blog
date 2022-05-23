180381506af6e0f1aa55c71ffae11e3918ffe33a
Mário Santos <mariodev7@gmail.com>
2022-05-23 03:39:57 +0000

    create styles of home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
 
 import Header from '../components/Header';
+import Post from '../components/Post';
 
 import { getPrismicClient } from '../services/prismic';
 
-import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 
 interface Post {
@@ -26,8 +27,35 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home() {
-  return <Header />;
+export default function Home(): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>Home - SpaceTraveling</title>
+      </Head>
+
+      <Header />
+
+      <main className={styles.container}>
+        <Post
+          title="Como utilizar Hooks"
+          subtitle="Pensando em sincronização em vez de ciclos de vida."
+          date="2021-03-15T19:25:28+0000"
+          author="Joseph Oliveira"
+        />
+        <Post
+          title="Criando um app CRA do zero"
+          subtitle="Tudo sobre como criar a sua primeira aplicação utilizando Create React App"
+          date="2021-03-15T19:25:28+0000"
+          author="Danilo Vieira"
+        />
+
+        <button type="button" className={styles.buttonLoadMorePosts}>
+          Carregar mais posts
+        </button>
+      </main>
+    </>
+  );
 }
 
 // export const getStaticProps = async () => {
